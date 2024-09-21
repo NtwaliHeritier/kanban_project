5e2ecab1ea20ffae170523d79e8ee677e6640a03
ntwali <ntwalihatsor78.nh@gmail.com>
2024-09-22 00:58:43 +0200

    fix compile

--- lib/kanban_project_web/gettext.ex
+++ lib/kanban_project_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule KanbanProjectWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :kanban_project
+  use Gettext, backend: KanbanProject.Gettext
 end
