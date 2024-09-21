18897da5a3d762abd8b46de92ed76edbf8a87bf2
ntwali <ntwalihatsor78.nh@gmail.com>
2024-09-22 01:03:37 +0200

    fix password issues

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :kanban_project, KanbanProject.Repo,
-  username: "hatsor",
-  password: "hatsor",
+  username: "postgres",
+  password: "postgres",
   hostname: "localhost",
   database: "kanban_project_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
